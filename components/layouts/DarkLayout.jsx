8b5f3955ae9ffbd256d48efb80ef2635677c2b24
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2022-03-30 11:16:01 -0600

    Basicos de nextjs

---
+++ components/layouts/DarkLayout.jsx
@@ -0,0 +1,16 @@
+const DarkLayout = ({ children }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "rgba(0,0,0,0.3)",
+        borderRadius: "10px",
+        padding: "10px",
+      }}
+    >
+      <h3>Dark-Layout</h3>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default DarkLayout;
---
+++ components/layouts/MainLayout.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Head from "next/head";
+import Navbar from "../Navbar";
+import styles from "./MainLayout.module.css";
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home - Sitio</title>
+        <meta name='description' content='Home Page' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <Navbar />
+
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+};
+
+export default MainLayout;
